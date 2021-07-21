8b03afb15bffa29509f8bde377918c02dbf821ab
Dullz95 <82204773+Dullz95@users.noreply.github.com>
2021-07-21 11:13:24 +0200

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
-    return "<strong>Random joke from chuck norris: </strong>" + response['value']
+    return  response
 
 if __name__=="__main__":
     app.debug=True
