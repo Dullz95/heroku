133c43207b66f884d514a66676383bd37dc0a895
Dullz95 <82204773+Dullz95@users.noreply.github.com>
2021-07-21 11:45:21 +0200

    update

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
     return  response
+@app.route('/catagories',methods=['GET'])
+def get_catagories():
+    api_url = "https://api.chucknorris.io/jokes/categories"
+    response = requests.get(api_url).json()
+    return '<pre style="word-wrap: break-word; white-space: pre-wrap;">{}</pre>'.format(response)
 
 if __name__=="__main__":
     app.debug=True
